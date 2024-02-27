89898fe4935ba5e24309be4c822d0dbed93e81e9
yoonhyunjin02 <97629676+yoonhyunjin02@users.noreply.github.com>
2024-02-27 19:43:35 +0900

    chater_8

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
-const express = require('express')
-const cors= require('cors');
-const app = express()
-const port = 3000
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'database.sqlite'
+});
 
-app.use(cors())
-
-app.get('/sound/:name', (req, res) => {
-    const { name } = req.params
-    
-    if(name == "dog"){
-        res.json({'sound' : "멍멍"})
-    } else if(name == "cat"){
-        res.json({'sound' : "야옹"})
-    } else if(name == "pig"){
-        res.json({'sound' : "꿀꿀"})
-    } else {
-        res.json({'sound' : "없음"})
+const Comments = sequelize.define('Comments', {
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
-})
+}, {
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+(async () => {
+    await Comments.sync();
+})();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs');
+
+/*read*/
+app.get('/', async function (req, res) {
+    const comments = await Comments.findAll();
+    res.render('index', { comments: comments });
+});
+
+/*create*/
+app.post('/create', async function (req, res) {
+    const { content } = req.body
+    await Comments.create({ content: content });
+    res.redirect('/')
+});
+
+/*update*/
+app.post('/update/:id', async function (req, res) {
+    const { content } = req.body
+    const { id } = req.params
+    await Comments.update({ content: content }, {
+        where: {
+            id: id
+        }
+    });
+    res.redirect('/')
+});
+
+/*delete*/
+app.post('/delete/:id', async function (req, res) {
+    const { id } = req.params
+    await Comments.destroy({
+        where: {
+            id: id
+        }
+    });
+    res.redirect('/')
+});
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
